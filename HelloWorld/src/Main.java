b736d672959a86925c6115c6e4d0aa54f76c9810
FerryRiet <Ferry.Rietveld@gmail.com>
2013-11-04 22:06:49 +0100

    Update helloWorld

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,7 +2,22 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!\n");
+		int doo = 10 ;
+		// should count one
+		if ( doo == 10 ) {
+			System.out.println("do == 10");
+		}
+		else {
+			System.out.println("do == 10");			
+		}
+		new Main().printIt("Hello world!\n");
+	}
+	public int printIt(String message) {
+		
+		System.out.println(message);
+		// Count me
+		//
+		return 1 ;
 	}
 
 }
